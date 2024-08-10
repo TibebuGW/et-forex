2fac442884d778bca38502a8300d767ece31c314
Tibebu <tibebugwas@gmail.com>
2024-08-10 13:03:40 +0000

    feat(web): logo styling

--- web/components/general/Navbar.tsx
+++ web/components/general/Navbar.tsx
@@ -6,7 +6,7 @@ import Logo from "@/public/assets/logo.png";
 import { useTheme } from "next-themes";
 
 const Navbar = () => {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState<boolean>(false);
 
   useEffect(() => {
@@ -20,7 +20,18 @@ const Navbar = () => {
       <div className="flex flex-row justify-between w-full h-16 py-2 lg:px-8 px-4 pt-4 ">
         <div className="flex items-center gap-4 mx-auto">
           <Image src={Logo} alt="Logo" height={80} width={80} className="object-cover" />
-          <p className="text-xl font-bold">ET Forex</p>
+          <p
+            style={{
+              fontSize: "20px",
+              color: theme === "dark" ? "white" : "black",
+              textShadow: theme
+                ? "1px 1px 0 #00ffff, -1px -1px 0 #ff00ff"
+                : "1px 1px 0 #ff00ff, -1px -1px 0 #00ffff",
+              fontWeight: "bold",
+            }}
+          >
+            ET Forex
+          </p>
         </div>
       </div>
     </div>
